5ebdadc36d124d3d1f0cb0b4ced1f393adddd576
Alexander Cherepnev <prog@shuric.com>
2022-12-12 22:20:02 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace GeometryLib
+{
+    public abstract class TGeometryRoot
+    {
+        public abstract double GetResult();
+    }
+
+    public class TTriangle : TGeometryRoot
+    {
+        public double dA, dB, dC;
+        public TTriangle (double a,double b, double c)
+        {
+            dA = a;
+            dB = b;
+            dC = c;
+        }
+        override public double GetResult()
+        {
+            double p = (dA + dB + dC) / 2;
+
+            return Math.Sqrt(p * (p - dA) * (p - dB) * (p - dC));
+
+        }
+    }
+
+    public class TCircle:TGeometryRoot
+    {
+        public double dR;
+        public TCircle(double r)
+        {
+            dR = r;
+        }
+        public override double GetResult()
+        {
+            return Math.PI * dR * dR;
+        }
+    }
+}
